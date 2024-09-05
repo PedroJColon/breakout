a3b917a5eabda8fe5acbecf6ea07b41cefc45793
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-09-05 17:45:03 -0400

    Create foundation for Shader Class
    Foundation for Shader Class made. Refactoring to
    occur soon.

--- include/Shader.h
+++ include/Shader.h
@@ -2,6 +2,9 @@
 #define SHADER_H
 
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -11,21 +14,30 @@
 class Shader
 {
 private:
-    /* data */
+    // MEMBER VARIABLES
+    std::string m_fragmentCode;
+    std::string m_vertexCode;
+    unsigned int m_ID;
 public:
-    Shader(/* args */);
+    Shader(std::string&, std::string&);
     ~Shader();
 
+    void ActivateShader();
+    void DeactivateShader();
     // UTILITY FUNCTIONS (MATH-RELATED)
-    void SetInt();
-    void SetFloat();
-    void SetVector2f();
-    void SetVector3f();
-    void SetVector4f();
-    void SetMatrix4f();
+    // void SetInt(const std::string&, int value) const;
+    // void SetFloat(const std::string&, float value) const;
+    // void SetVector2f(const std::string&, glm::vec2) const;
+    // void SetVector3f(const std::string&, glm::vec3) const;
+    // void SetVector4f(const std::string&, glm::vec4) const;
+    // void SetMatrix4f(const std::string&, glm::mat4) const;
+
 private:
-    // Check compliation error
-    //
+    // UTILITY FUNCTIONS
+    void CheckCompileErrors(unsigned int, std::string);
+    void CheckLinkingErrors();
+    // HANDLE SHADER CREATION AND COMPLIATION PROCESS
+    void CompileShaders();
 };
 
 #endif // SHADER_H
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,1 +1,100 @@
 #include "Shader.h"
+
+Shader::Shader(std::string& vertexPath, std::string& fragmentPath)
+{
+    std::ifstream vertexFile;
+    std::ifstream fragmentFile;
+
+    vertexFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragmentFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        vertexFile.open(vertexPath);
+        fragmentFile.open(fragmentPath);
+        std::stringstream vertexStream, fragmentStream;
+        vertexStream << vertexFile.rdbuf();
+        fragmentStream << fragmentFile.rdbuf();
+
+        vertexFile.close();
+        fragmentFile.close();
+
+        m_vertexCode = vertexStream.str();
+        m_fragmentCode = fragmentStream.str();
+        
+    }
+    catch(const std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_READ" << std::endl;  
+    }
+
+    CompileShaders();
+}
+
+Shader::~Shader()
+{
+}
+
+void Shader::ActivateShader()
+{
+    glUseProgram(m_ID);
+}
+
+void Shader::DeactivateShader()
+{
+    glUseProgram(0);
+}
+
+
+void Shader::CheckCompileErrors(unsigned int shader, std::string type)
+{
+    int success;
+    char infoLog[512];
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::" << type <<"::COMPLIATION_FAILE\n" << std::endl;
+    }
+}
+
+void Shader::CheckLinkingErrors()
+{
+    int success;
+    char infoLog[512];
+
+    glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(m_ID, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << std::endl;   
+    }
+    
+}
+
+void Shader::CompileShaders()
+{
+    unsigned int vertex, fragment;
+
+    const char* vertexCode = m_vertexCode.c_str();
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertexCode, NULL);
+    glCompileShader(vertex);
+    CheckCompileErrors(vertex, "VERTEX");
+
+    const char* fragmentCode = m_fragmentCode.c_str();
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragmentCode, NULL);
+    glCompileShader(fragment);
+    CheckCompileErrors(fragment, "FRAGMENT");
+
+    m_ID = glCreateProgram();
+    glAttachShader(m_ID, vertex);
+    glAttachShader(m_ID, fragment);
+    glLinkProgram(m_ID);
+    CheckLinkingErrors();
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
