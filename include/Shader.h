68c62ace1a638922bcd418781817cb569f98329b
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-09-05 17:49:44 -0400

    Add Shader Class to CMakeLists
    Add shader class to compile with CMakeLists and
    fixed include issues in Shader.h

--- include/Shader.h
+++ include/Shader.h
@@ -7,9 +7,9 @@
 #include <iostream>
 
 #include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <../glm/glm.hpp>
+#include <../glm/gtc/matrix_transform.hpp>
+#include <../glm/gtc/type_ptr.hpp>
 
 class Shader
 {
