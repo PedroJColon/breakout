929df8935b632bccf3b778a3d7f35d06d342a982
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-09-15 19:01:31 -0400

    Update Texture2D
    Texture generation now done with Constructor, along with additional comments made to better document and note functions and variables

--- include/Texture2D.h
+++ include/Texture2D.h
@@ -8,17 +8,18 @@ class Texture2D
 private:
 public:
     unsigned int m_width, m_height;
-    unsigned int m_interalFormat;
+    unsigned int m_internalFormat;
     unsigned int m_imageFormat;
     // texture config
     unsigned int m_warpS;
     unsigned int m_warpT;
     unsigned int m_filterMin;
     unsigned int m_filterMax;
-    unsigned int m_ID;
-    Texture2D();
+
+    unsigned int m_ID; // Texture's ID to be used
+    Texture2D(); // Empty default constructor made to make linker happy - DO NOT USE!
+    Texture2D(int, int, unsigned char*, bool);
     ~Texture2D();
-    void Generate(int, int, unsigned char*);
     void Bind() const;
 };
 
--- src/Texture2D.cpp
+++ src/Texture2D.cpp
@@ -1,32 +1,49 @@
 #include "Texture2D.h"
 
-Texture2D::Texture2D() : m_width(0), m_height(0), m_interalFormat(GL_RGB), m_imageFormat(GL_RGB), m_warpS(GL_REPEAT), m_warpT(GL_REPEAT),
-m_filterMin(GL_LINEAR), m_filterMax(GL_LINEAR)
+Texture2D::Texture2D()
 {
+    // Set inital variables (to have warnings go away)
+    m_ID = 0;
+    m_width = 0;
+    m_height = 0;
+    m_internalFormat = GL_RGB;
+    m_imageFormat = GL_RGB;
+    m_warpS = GL_REPEAT;
+    m_warpT = GL_REPEAT;
+    m_filterMin = GL_LINEAR;
+    m_filterMax = GL_LINEAR;
+}
+
+Texture2D::Texture2D(int width, int height, unsigned char* data, bool alpha) : m_width(width), m_height(height), m_internalFormat(GL_RGB), 
+m_imageFormat(GL_RGB), m_warpS(GL_REPEAT), m_warpT(GL_REPEAT), m_filterMin(GL_LINEAR), m_filterMax(GL_LINEAR)
+{
+    // Generate the texture with its own ID
     glGenTextures(1, &m_ID);
+    // If we have alpha value, override the current initalize values to use RGBA values
+    if (alpha)
+    {
+        m_internalFormat = GL_RGBA;
+        m_imageFormat = GL_RGBA;
+    }
+    // Ensure the texture is bind to set parameters
+    glBindTexture(GL_TEXTURE_2D, m_ID);
+    // Create texture using image data made with STBI
+    glTexImage2D(GL_TEXTURE_2D, 0, m_internalFormat, m_width, m_height, 0, m_imageFormat, GL_UNSIGNED_BYTE, data);
+    // Set texture parameters with the following member variables initalized
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_warpS);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_warpT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_filterMin);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_filterMax);
+    // Unbind texture once we set parameters
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 Texture2D::~Texture2D()
 {
 }
 
-void Texture2D::Generate(int width, int height, unsigned char* data)
-{
-    m_width = width;
-    m_height = height;
-
-    glBindTexture(GL_TEXTURE_2D, m_ID);
-    glTexImage2D(GL_TEXTURE_2D, 0, m_interalFormat, m_width, m_height, 0, m_imageFormat, GL_UNSIGNED_BYTE, data);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_warpS);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_warpT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_filterMin);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_filterMax);
-    
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
 void Texture2D::Bind() const
 {
+    // Use texture associated with its ID
     glBindTexture(GL_TEXTURE_2D, m_ID);
 }
