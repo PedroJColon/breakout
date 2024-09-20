b73258c0f359ad0b24ff9f8ad67ecaf7ad2bfbe5
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-09-20 16:31:57 -0400

    Update Shader Class
    Shader Class updated to remove inefficient way of generating shaders

--- include/Shader.h
+++ include/Shader.h
@@ -19,8 +19,7 @@ public:
     unsigned int m_ID; // Shader Program ID - Important for us to free shader resource when needed
 
     // CONSTRUCTORS
-    Shader(); // Empty default constructor made to make linker happy - Will not be used often if at all
-    Shader(std::string&, std::string&); // This constructor will be used to create and compile shaders into a shader program
+    Shader();
     // DESTRUCTORS
     ~Shader();
 
@@ -31,7 +30,7 @@ public:
     void SetInt(const std::string&, int) const;
     // void SetFloat(const std::string&, float) const;
     // void SetVector2f(const std::string&, glm::vec2) const;
-    // void SetVector3f(const std::string&, glm::vec3) const;
+    void SetVector3f(const std::string&, glm::vec3) const;
     // void SetVector4f(const std::string&, glm::vec4) const;
     void SetMatrix4f(const std::string&, glm::mat4) const;
 
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -5,38 +5,6 @@ Shader::Shader()
     m_ID = 0;
 }
 
-Shader::Shader(std::string& shaderVertexCode, std::string& shaderFragmentCode)
-{
-    //unsigned int vertex, fragment;
-
-    //// Has to be converted to const char due to how GLAD glShaderSource works (Gives errors if we use string)
-    //const char* vertexCode = shaderVertexCode.c_str();
-    //// Compile our Vertex shader now
-    //vertex = glCreateShader(GL_VERTEX_SHADER);
-    //glShaderSource(vertex, 1, &vertexCode, NULL);
-    //glCompileShader(vertex);
-    //CheckCompileErrors(vertex, "VERTEX");
-
-    //// Has to be converted to const char due to how GLAD glShaderSource works (Gives errors if we use string)
-    //const char* fragmentCode = shaderFragmentCode.c_str();
-    //// Compile our Fragment shader now
-    //fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    //glShaderSource(fragment, 1, &fragmentCode, NULL);
-    //glCompileShader(fragment);
-    //CheckCompileErrors(fragment, "FRAGMENT");
-
-    //// Once we have complied our shaders, attach shaders to program and link them together
-    //m_ID = glCreateProgram();
-    //glAttachShader(m_ID, vertex);
-    //glAttachShader(m_ID, fragment);
-    //glLinkProgram(m_ID);
-    //CheckLinkingErrors();
-
-    //// With our program made, delete shaders to save on space
-    //glDeleteShader(vertex);
-    //glDeleteShader(fragment);
-}
-
 Shader::~Shader()
 {
 }
@@ -93,6 +61,11 @@ void Shader::SetInt(const std::string& name, int value) const
     glUniform1i(glGetUniformLocation(m_ID, name.c_str()), value);
 }
 
+
+void Shader::SetVector3f(const std::string& name, glm::vec3 value) const
+{
+    glUniform3fv(glGetUniformLocation(m_ID, name.c_str()), 1, glm::value_ptr(value));
+}
 
 void Shader::SetMatrix4f(const std::string& name, glm::mat4 value) const
 {
