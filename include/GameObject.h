847197cea933c404f3692c31997dc975468264c1
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-10-05 00:30:54 -0400

    Add additional functionality to GameObject

--- include/GameObject.h
+++ include/GameObject.h
@@ -3,9 +3,19 @@
 
 #include <glad/glad.h>
 #include <glm.hpp>
+#include <vector>
+#include <memory>
 
-#include "Texture2D.h"
+#include "BaseComponent.h"
 #include "Renderer.h"
+
+enum ID
+{
+	PLAYER,
+	BALL,
+	SOLIDBRICK,
+	BRICK,
+};
 
 class GameObject
 {
@@ -13,12 +23,22 @@ public:
 	glm::vec2 m_pos, m_size, m_velocity;
 	glm::vec3 m_color;
 	float m_rotation;
+	float m_radius;
+	float m_stuck;
 	bool m_isSolid;
 	bool m_destroyed;
 
+	ID m_ID;
+
+	std::vector<std::shared_ptr<IBaseComponent>> m_component;
+
 	GameObject();
-	GameObject(glm::vec2, glm::vec2, glm::vec3 = glm::vec3(1.0f), glm::vec2 = glm::vec2(0.0f, 0.0f));
+	GameObject(glm::vec2, glm::vec2, ID, glm::vec3 = glm::vec3(1.0f), glm::vec2 = glm::vec2(0.0f, 0.0f));
 	~GameObject();
+
+	glm::vec2 Move(float, unsigned int);
+	void Reset(glm::vec2, glm::vec2);
+	void AddComponent(std::shared_ptr<IBaseComponent>);
 
 private:
 
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,15 +1,58 @@
-#include <GameObject.h>
+#include "GameObject.h"
+#include "GameObject.h"
 
 GameObject::GameObject() : m_pos(0), m_size(1), m_velocity(0.0f), m_color(1.0f), m_rotation(0.0f), 
 m_isSolid(false), m_destroyed(false)
 {
+	m_radius = 0.0f;
+	m_stuck = false;
 }
 
-GameObject::GameObject(glm::vec2 pos, glm::vec2 size, glm::vec3 color, glm::vec2 velocity) : m_pos(pos), m_size(size), m_color(color),
-m_velocity(velocity), m_rotation(0.0f), m_isSolid(false), m_destroyed(false)
+GameObject::GameObject(glm::vec2 pos, glm::vec2 size, ID objectID, glm::vec3 color, glm::vec2 velocity)  : m_pos(pos), m_size(size), m_color(color),
+m_velocity(velocity), m_rotation(0.0f), m_isSolid(false), m_destroyed(false), m_ID(objectID)
 {
+	m_radius = 0.0f;
+	m_stuck = false;
 }
 
 GameObject::~GameObject()
 {
+}
+
+glm::vec2 GameObject::Move(float deltaTime, unsigned int windowWidth)
+{
+	if (!m_stuck)
+	{
+		m_pos += m_velocity * deltaTime;
+
+		if (m_pos.x <= 0.0f)
+		{
+			m_velocity.x = -m_velocity.x;
+			m_pos.x = 0.0f;
+		}
+		else if (m_pos.x + m_size.x >= windowWidth)
+		{
+			m_velocity.x = -m_velocity.x;
+			m_pos.x = windowWidth - m_size.x;
+		}
+		if (m_pos.y <= 0.0f)
+		{
+			m_velocity.y = -m_velocity.y;
+			m_pos.y = 0.0f;
+		}
+	}
+
+	return m_pos;
+}
+
+void GameObject::Reset(glm::vec2 pos, glm::vec2 velocity)
+{
+	m_pos = pos;
+	m_velocity = velocity;
+	m_stuck = true;
+}
+
+void GameObject::AddComponent(std::shared_ptr<IBaseComponent> comp)
+{
+	m_component.push_back(comp);
 }
